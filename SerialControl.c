5e4ddd386148bce037d408431b9db2a37db32066
Madhav-S <madhav.srini@gmail.com>
2013-06-01 19:08:18 -0700

    Fixed GPS Baud Rate

--- SerialControl.c
+++ SerialControl.c
@@ -46,8 +46,8 @@ int openGPSSerial(int* desc){
 	}
 	else{
 		tcgetattr(*desc, &options);
-		cfsetispeed(&options, B57600);
-		cfsetospeed(&options, B57600);
+		cfsetispeed(&options, B38400);
+		cfsetospeed(&options, B38400);
 		options.c_cflag |= (CLOCAL |CREAD);
 		tcsetattr(*desc, TCSANOW, &options);
 		return 0;
